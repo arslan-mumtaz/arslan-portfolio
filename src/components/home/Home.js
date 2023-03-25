dba57a40dccd0afc9a14f44f280ec0faba0d1537
Arslan <arslan.mumtaz04@gmail.com>
2023-03-26 02:46:42 +0500

    Image updated
    arslan

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Style from './Home.module.scss';
-import me from '../../img/self.png';
+import me from '../../img/self1.png';
 import classNames from 'classnames';
 import EmojiBullet from "./EmojiBullet";
 import SocialIcon from "./SocialIcon";
